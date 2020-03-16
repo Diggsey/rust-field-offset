b573aa2a3c69298bed76b7712c3f80929897ffd2
Diggory Blake <diggsey@googlemail.com>
2020-03-16 21:16:50 +0000

    Fix clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+// Explicit lifetimes are clearer when we are working with raw pointers,
+// as the compiler will not warn us if we specify lifetime constraints
+// which are too lax.
+#![allow(clippy::needless_lifetimes)]
+
 use std::marker::PhantomData;
 use std::mem;
 use std::ops::Add;
@@ -35,7 +40,15 @@ impl<T, U> FieldOffset<T, U> {
     /// Construct a field offset via a lambda which returns a reference
     /// to the field in question.
     ///
-    /// The lambda *must not* access the value passed in.
+    /// # Safety
+    /// 
+    /// The lambda *must not* dereference the provided pointer or access the
+    /// inner value in any way as it may point to uninitialized memory.
+    /// 
+    /// For the returned `FieldOffset` to be safe to use, the returned pointer
+    /// must be valid for *any* instance of `T`. For example, returning a pointer
+    /// to a field from an enum with multiple variants will produce a `FieldOffset`
+    /// which is unsafe to use.
     pub unsafe fn new<F: for<'a> FnOnce(*const T) -> *const U>(f: F) -> Self {
         let offset = Self::with_uninit_ptr(|base_ptr| {
             let field_ptr = f(base_ptr);
@@ -46,6 +59,13 @@ impl<T, U> FieldOffset<T, U> {
         Self::new_from_offset(offset)
     }
     /// Construct a field offset directly from a byte offset.
+    /// 
+    /// # Safety
+    /// 
+    /// For the returned `FieldOffset` to be safe to use, the field offset
+    /// must be valid for *any* instance of `T`. For example, returning the offset
+    /// to a field from an enum with multiple variants will produce a `FieldOffset`
+    /// which is unsafe to use.
     #[inline]
     pub unsafe fn new_from_offset(offset: usize) -> Self {
         // Sanity check: ensure that the field offset plus the field size
@@ -58,59 +78,99 @@ impl<T, U> FieldOffset<T, U> {
 
         FieldOffset(offset, PhantomData)
     }
+
     // Methods for applying the pointer to member
+
     /// Apply the field offset to a native pointer.
     #[inline]
-    pub fn apply_ptr<'a>(&self, x: *const T) -> *const U {
+    pub fn apply_ptr(self, x: *const T) -> *const U {
         ((x as usize) + self.0) as *const U
     }
     /// Apply the field offset to a native mutable pointer.
     #[inline]
-    pub fn apply_ptr_mut<'a>(&self, x: *mut T) -> *mut U {
+    pub fn apply_ptr_mut(self, x: *mut T) -> *mut U {
         ((x as usize) + self.0) as *mut U
     }
     /// Apply the field offset to a reference.
     #[inline]
-    pub fn apply<'a>(&self, x: &'a T) -> &'a U {
+    pub fn apply<'a>(self, x: &'a T) -> &'a U {
         unsafe { &*self.apply_ptr(x) }
     }
     /// Apply the field offset to a mutable reference.
     #[inline]
-    pub fn apply_mut<'a>(&self, x: &'a mut T) -> &'a mut U {
+    pub fn apply_mut<'a>(self, x: &'a mut T) -> &'a mut U {
         unsafe { &mut *self.apply_ptr_mut(x) }
     }
     /// Get the raw byte offset for this field offset.
     #[inline]
-    pub fn get_byte_offset(&self) -> usize {
+    pub fn get_byte_offset(self) -> usize {
         self.0
     }
+
     // Methods for unapplying the pointer to member
+
     /// Unapply the field offset to a native pointer.
-    ///
+    /// 
+    /// # Safety
+    /// 
     /// *Warning: very unsafe!*
+    /// 
+    /// This applies a negative offset to a pointer. If the safety
+    /// implications of this are not already clear to you, then *do
+    /// not* use this method. Also be aware that Rust has stronger
+    /// aliasing rules than other languages, so it may be UB to
+    /// dereference the resulting pointer even if it points to a valid
+    /// location, due to the presence of other live references.
     #[inline]
-    pub unsafe fn unapply_ptr<'a>(&self, x: *const U) -> *const T {
+    pub unsafe fn unapply_ptr(self, x: *const U) -> *const T {
         ((x as usize) - self.0) as *const T
     }
     /// Unapply the field offset to a native mutable pointer.
     ///
+    /// # Safety
+    /// 
     /// *Warning: very unsafe!*
+    /// 
+    /// This applies a negative offset to a pointer. If the safety
+    /// implications of this are not already clear to you, then *do
+    /// not* use this method. Also be aware that Rust has stronger
+    /// aliasing rules than other languages, so it may be UB to
+    /// dereference the resulting pointer even if it points to a valid
+    /// location, due to the presence of other live references.
     #[inline]
-    pub unsafe fn unapply_ptr_mut<'a>(&self, x: *mut U) -> *mut T {
+    pub unsafe fn unapply_ptr_mut(self, x: *mut U) -> *mut T {
         ((x as usize) - self.0) as *mut T
     }
     /// Unapply the field offset to a reference.
     ///
+    /// # Safety
+    /// 
     /// *Warning: very unsafe!*
+    /// 
+    /// This applies a negative offset to a reference. If the safety
+    /// implications of this are not already clear to you, then *do
+    /// not* use this method. Also be aware that Rust has stronger
+    /// aliasing rules than other languages, so this method may cause UB
+    /// even if the resulting reference points to a valid location, due
+    /// to the presence of other live references.
     #[inline]
-    pub unsafe fn unapply<'a>(&self, x: &'a U) -> &'a T {
+    pub unsafe fn unapply<'a>(self, x: &'a U) -> &'a T {
         &*self.unapply_ptr(x)
     }
     /// Unapply the field offset to a mutable reference.
     ///
+    /// # Safety
+    /// 
     /// *Warning: very unsafe!*
+    /// 
+    /// This applies a negative offset to a reference. If the safety
+    /// implications of this are not already clear to you, then *do
+    /// not* use this method. Also be aware that Rust has stronger
+    /// aliasing rules than other languages, so this method may cause UB
+    /// even if the resulting reference points to a valid location, due
+    /// to the presence of other live references.
     #[inline]
-    pub unsafe fn unapply_mut<'a>(&self, x: &'a mut U) -> &'a mut T {
+    pub unsafe fn unapply_mut<'a>(self, x: &'a mut U) -> &'a mut T {
         &mut *self.unapply_ptr_mut(x)
     }
 }
